89b15c0e736a86fa2e36fdd2fb67094ec367221b
FacundoBraga <facundobraga@hotmail.com>
2023-12-13 00:15:01 -0300

    Big Card mockup.

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,12 +2,17 @@
 // import { useNavigate } from "react-router-dom";
 // import { useSelector } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react";
 
-import ProductsCarousel from "../../components/products-carousel/products-carousel.component";
-import PromoCarousel from "../../components/promo-carousel/promo-carousel.component";
 import { getProducts, getProductsPromo } from "../../services/product.service";
 // import { setOrderID } from "../../store/features/order/orderSlice";
 
@@ -20,6 +25,9 @@ import JoyrideTutorial from "../../utils/joyRideTutorial";
 import ReactGA from "react-ga4";
 import { getCustomerCards } from "../../services/customer.service";
 import { getStore } from "../../services/store.service";
+
+import { ReactComponent as TrendLineChartIcon } from "../../assets/images/trend-line-chart.svg";
+import { ReactComponent as ArrowBigGreen } from "../../assets/images/arrow-green-big.svg";
 
 const HomeBody = ({ storeId, cardId }) => {
   // const navigate = useNavigate();
@@ -103,32 +111,179 @@ const HomeBody = ({ storeId, cardId }) => {
         }}
       >
         <Grid container>
-          <Grid item xs={12} marginBottom="1rem">
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 800,
-                margin: "10px 0 0 15px",
-                color: "pink.main",
-              }}
-            >
-              Promociones
-            </Typography>
-            <PromoCarousel />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 800,
-                margin: "10px 0 0 15px",
-                color: "grey.main",
-              }}
-            >
-              Catálogo de Precios
-            </Typography>
-            <ProductsCarousel />
+          <Grid
+            item
+            xs={12}
+            sx={{
+              marginInline: "1rem",
+              marginBlock: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              width: "350px",
+              height: "auto",
+            }}
+          >
+            <Paper sx={{ borderRadius: "10px" }} elevation={1}>
+              <Grid
+                item
+                xs={12}
+                sx={{
+                  minHeight: "89px",
+                  backgroundColor: "#663B73",
+                  borderRadius: "10px 10px 0px 0px",
+                  padding: "16px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    color: "#FCFCF7",
+                    fontSize: "24px",
+                  }}
+                >
+                  Ingresos{" "}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontWeight: "normal",
+                    color: "#A9A9A9",
+                    fontSize: "18px",
+                  }}
+                >
+                  Marina de Empresas{" "}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sx={{
+                  height: "207px",
+                  padding: "32px",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Grid
+                  xs={12}
+                  sx={{
+                    minHeight: "2rem",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <Grid
+                    xs={2}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <TrendLineChartIcon style={{ height: "32px" }} />
+                  </Grid>
+                  <Grid
+                    xs={8}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <Typography sx={{ fontSize: "1.25rem" }}>
+                      Ingresos Brutos
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid
+                  xs={12}
+                  sx={{
+                    minHeight: "4rem",
+                    display: "flex",
+                  }}
+                >
+                  <Grid
+                    xs={2}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <ArrowBigGreen />
+                  </Grid>
+                  <Grid
+                    xs={8}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    {" "}
+                    <Typography
+                      sx={{
+                        fontSize: "4rem",
+                        lineHeight: "4rem",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      768
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontSize: "2rem",
+                        display: "flex",
+                        alignItems: "flex-end",
+                      }}
+                    >
+                      €
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    xs={2}
+                    sx={{
+                      color: "#fff",
+                      display: "flex",
+                      alignItems: "end",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontSize: "1rem",
+                        backgroundColor: "#2CAF5F",
+                        borderRadius: "3px",
+                        padding: "5px",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      71%
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid
+                  xs={12}
+                  sx={{
+                    minHeight: "2rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "end",
+                  }}
+                >
+                  <ButtonGroup
+                    variant="outlined"
+                    color="black"
+                    sx={{ height: "25px" }}
+                    size="small"
+                  >
+                    <Button value="1W">1W</Button>
+                    <Button value="1M">1M</Button>
+                    <Button value="1Y">1Y</Button>
+                    <Button value="All">All</Button>
+                  </ButtonGroup>
+                </Grid>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sx={{
+                  minHeight: "35px",
+                  backgroundColor: "#663B73",
+                  borderRadius: "0px 0px 10px 10px",
+                }}
+              ></Grid>
+            </Paper>
           </Grid>
         </Grid>
       </Box>
