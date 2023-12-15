96b218034dd914a47f32cd20ad1c913819c7a177
FacundoBraga <facundobraga@hotmail.com>
2023-12-14 23:49:45 -0300

    Componentizing Big Card.

---
+++ src/components/big-card/big-card.component.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import { Button, ButtonGroup, Grid, Paper, Typography } from "@mui/material";
+import { ReactComponent as TrendLineChartIcon } from "../../assets/images/trend-line-chart.svg";
+import { ReactComponent as ArrowBigGreen } from "../../assets/images/arrow-green-big.svg";
+
+const BigCard = ({
+  title,
+  subtitle,
+  headline,
+  quantity,
+  currencySymbol,
+  percentage,
+}) => {
+  return (
+    <Grid container>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          marginInline: "1rem",
+          marginBlock: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          width: "350px",
+          height: "auto",
+        }}
+      >
+        <Paper sx={{ borderRadius: "10px" }} elevation={5}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              minHeight: "89px",
+              backgroundColor: "#663B73",
+              borderRadius: "10px 10px 0px 0px",
+              padding: "16px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+            }}
+          >
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                color: "#FCFCF7",
+                fontSize: "24px",
+              }}
+            >
+              {title}
+            </Typography>
+            <Typography
+              sx={{
+                fontWeight: "normal",
+                color: "#A9A9A9",
+                fontSize: "18px",
+              }}
+            >
+              {subtitle}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              height: "207px",
+              padding: "32px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Grid
+              xs={12}
+              sx={{
+                minHeight: "2rem",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Grid xs={2} sx={{ display: "flex", justifyContent: "center" }}>
+                <TrendLineChartIcon style={{ height: "32px" }} />
+              </Grid>
+              <Grid xs={8} sx={{ display: "flex", justifyContent: "center" }}>
+                <Typography sx={{ fontSize: "1.25rem" }}>{headline}</Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              xs={12}
+              sx={{
+                minHeight: "4rem",
+                display: "flex",
+              }}
+            >
+              <Grid xs={2} sx={{ display: "flex", justifyContent: "center" }}>
+                <ArrowBigGreen />
+              </Grid>
+              <Grid xs={8} sx={{ display: "flex", justifyContent: "center" }}>
+                {" "}
+                <Typography
+                  sx={{
+                    fontSize: "4rem",
+                    lineHeight: "4rem",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {quantity}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "2rem",
+                    display: "flex",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  {currencySymbol}
+                </Typography>
+              </Grid>
+              <Grid
+                xs={2}
+                sx={{
+                  color: "#fff",
+                  display: "flex",
+                  alignItems: "end",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: "1rem",
+                    backgroundColor: "#2CAF5F",
+                    borderRadius: "3px",
+                    padding: "5px",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {percentage}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              xs={12}
+              sx={{
+                minHeight: "2rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "end",
+              }}
+            >
+              <ButtonGroup
+                variant="outlined"
+                color="black"
+                sx={{ height: "25px" }}
+                size="small"
+              >
+                <Button value="1W">1W</Button>
+                <Button value="1M">1M</Button>
+                <Button value="1Y">1Y</Button>
+                <Button value="All">All</Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              minHeight: "35px",
+              backgroundColor: "#663B73",
+              borderRadius: "0px 0px 10px 10px",
+            }}
+          ></Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default BigCard;
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,93 +2,35 @@
 // import { useNavigate } from "react-router-dom";
 // import { useSelector } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Grid,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react";
 
 import { getProducts, getProductsPromo } from "../../services/product.service";
-// import { setOrderID } from "../../store/features/order/orderSlice";
 
 import Footer from "../../components/footer/footer.component";
 import Header from "../../components/header/header.component";
-// import { getCustomerCards } from "../../services/customer.service";
-// import { createOrder } from "../../services/order.service";
-// import { SignalRContext } from "../..";
+
 import JoyrideTutorial from "../../utils/joyRideTutorial";
 import ReactGA from "react-ga4";
 import { getCustomerCards } from "../../services/customer.service";
 import { getStore } from "../../services/store.service";
 
-import { ReactComponent as TrendLineChartIcon } from "../../assets/images/trend-line-chart.svg";
-import { ReactComponent as ArrowBigGreen } from "../../assets/images/arrow-green-big.svg";
+import BigCard from "../../components/big-card/big-card.component";
 
-const HomeBody = ({ storeId, cardId }) => {
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const { orderId } = useSelector((state) => state.order);
-  // const { customerCardsData } = useSelector((state) => state.customer);
-  // const [hasCreatedOrder, setHasCreatedOrder] = useState(false);
-
-  // SignalRContext.useSignalREffect(
-  //   "OrderStarted",
-  //   (message) => {
-  //     console.log("OrderStarted", message);
-  //     dispatch(setOrderID(message.orderId));
-  //     navigate("/order-created-success");
-  //   },
-  //   []
-  // );
-
-  // SignalRContext.useSignalREffect(
-  //   "OrderCancelled",
-  //   (message) => {
-  //     console.log("OrderCancelled", message);
-  //     navigate("/order-cancelled");
-  //   },
-  //   []
-  // );
-
-  // useEffect(() => {
-  //   const createOrderCall = async () => {
-  //     const order = await createOrder({ storeId, cardId });
-  //     setHasCreatedOrder(false);
-  //     if (order.status === 500) {
-  //       navigate("/order-cancelled");
-  //     }
-  //   };
-
-  //   if (!customerCardsData) return;
-
-  //   const cardSelected = customerCardsData.find(
-  //     (card) => card.id === parseInt(cardId)
-  //   );
-  //   if (!cardSelected || hasCreatedOrder) return;
-
-  //   const shouldCreateOrder = storeId && cardSelected.token && !orderId;
-
-  //   if (shouldCreateOrder) {
-  //     SignalRContext.on();
-  //     createOrderCall();
-  //     setHasCreatedOrder(true);
-  //   }
-  // }, [
-  //   cardId,
-  //   customerCardsData,
-  //   dispatch,
-  //   hasCreatedOrder,
-  //   navigate,
-  //   orderId,
-  //   storeId,
-  // ]);
-
+const HomeBody = () => {
   const [runTutorial, setRunTutorial] = useState(false);
+
+  //hardcoded fake data.
+  const bigCardData = {
+    title: "Ingresos",
+    subtitle: "Marina de Empresas",
+    headline: "Ingresos Brutos",
+    quantity: "768",
+    currencySymbol: "€",
+    percentage: "71%",
+  };
+
   return (
     <>
       <JoyrideTutorial runTutorial={runTutorial} />
@@ -110,182 +52,7 @@ const HomeBody = ({ storeId, cardId }) => {
           overflowX: "hidden",
         }}
       >
-        <Grid container>
-          <Grid
-            item
-            xs={12}
-            sx={{
-              marginInline: "1rem",
-              marginBlock: "1rem",
-              display: "flex",
-              flexDirection: "column",
-              width: "350px",
-              height: "auto",
-            }}
-          >
-            <Paper sx={{ borderRadius: "10px" }} elevation={1}>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  minHeight: "89px",
-                  backgroundColor: "#663B73",
-                  borderRadius: "10px 10px 0px 0px",
-                  padding: "16px",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-around",
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#FCFCF7",
-                    fontSize: "24px",
-                  }}
-                >
-                  Ingresos{" "}
-                </Typography>
-                <Typography
-                  sx={{
-                    fontWeight: "normal",
-                    color: "#A9A9A9",
-                    fontSize: "18px",
-                  }}
-                >
-                  Marina de Empresas{" "}
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  height: "207px",
-                  padding: "32px",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <Grid
-                  xs={12}
-                  sx={{
-                    minHeight: "2rem",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <Grid
-                    xs={2}
-                    sx={{ display: "flex", justifyContent: "center" }}
-                  >
-                    <TrendLineChartIcon style={{ height: "32px" }} />
-                  </Grid>
-                  <Grid
-                    xs={8}
-                    sx={{ display: "flex", justifyContent: "center" }}
-                  >
-                    <Typography sx={{ fontSize: "1.25rem" }}>
-                      Ingresos Brutos
-                    </Typography>
-                  </Grid>
-                </Grid>
-                <Grid
-                  xs={12}
-                  sx={{
-                    minHeight: "4rem",
-                    display: "flex",
-                  }}
-                >
-                  <Grid
-                    xs={2}
-                    sx={{ display: "flex", justifyContent: "center" }}
-                  >
-                    <ArrowBigGreen />
-                  </Grid>
-                  <Grid
-                    xs={8}
-                    sx={{ display: "flex", justifyContent: "center" }}
-                  >
-                    {" "}
-                    <Typography
-                      sx={{
-                        fontSize: "4rem",
-                        lineHeight: "4rem",
-                        overflow: "hidden",
-                        whiteSpace: "nowrap",
-                        textOverflow: "ellipsis",
-                      }}
-                    >
-                      768
-                    </Typography>
-                    <Typography
-                      sx={{
-                        fontSize: "2rem",
-                        display: "flex",
-                        alignItems: "flex-end",
-                      }}
-                    >
-                      €
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    xs={2}
-                    sx={{
-                      color: "#fff",
-                      display: "flex",
-                      alignItems: "end",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        fontSize: "1rem",
-                        backgroundColor: "#2CAF5F",
-                        borderRadius: "3px",
-                        padding: "5px",
-                        overflow: "hidden",
-                        whiteSpace: "nowrap",
-                        textOverflow: "ellipsis",
-                      }}
-                    >
-                      71%
-                    </Typography>
-                  </Grid>
-                </Grid>
-                <Grid
-                  xs={12}
-                  sx={{
-                    minHeight: "2rem",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "end",
-                  }}
-                >
-                  <ButtonGroup
-                    variant="outlined"
-                    color="black"
-                    sx={{ height: "25px" }}
-                    size="small"
-                  >
-                    <Button value="1W">1W</Button>
-                    <Button value="1M">1M</Button>
-                    <Button value="1Y">1Y</Button>
-                    <Button value="All">All</Button>
-                  </ButtonGroup>
-                </Grid>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  minHeight: "35px",
-                  backgroundColor: "#663B73",
-                  borderRadius: "0px 0px 10px 10px",
-                }}
-              ></Grid>
-            </Paper>
-          </Grid>
-        </Grid>
+        <BigCard {...bigCardData} />
       </Box>
       <Footer />
     </>
@@ -294,8 +61,6 @@ const HomeBody = ({ storeId, cardId }) => {
 
 const Home = () => {
   const dispatch = useDispatch();
-  // const [searchParams] = useSearchParams();
-  // const [params, setParams] = useState({});
   const { getProductsStatus, getProductsPromoStatus } = useSelector(
     (state) => state.products
   );
@@ -342,27 +107,6 @@ const Home = () => {
     setStoreId();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // useEffect(() => {
-  //   const storeId = searchParams.get("storeId") || "";
-  //   setParams({ storeId });
-  // }, [searchParams]);
-
-  // useEffect(
-  //   () => {
-  //     if (getProductsPromoStatus === "idle") {
-  //       dispatch(getProductsPromo({ storeId: params.storeId }));
-  //     }
-  //     if (getProductsStatus === "idle") {
-  //       dispatch(getProducts());
-  //     }
-  //     if (customerCardsStatus === "idle") {
-  //       dispatch(getCustomerCards());
-  //     }
-  //   },
-  //   // eslint-disable-next-line
-  //   []
-  // );
 
   return (
     <>
