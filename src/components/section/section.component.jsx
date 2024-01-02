df179f928675b3b8a84c98bdd1f06c15f0b97eea
FacundoBraga <facundobraga@hotmail.com>
2024-01-02 16:43:50 -0300

    Minor changes and dev babel dependency added.

--- src/components/section/section.component.jsx
+++ src/components/section/section.component.jsx
@@ -1,5 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
-import { ReactComponent as ArrowSmallGreen } from "../../assets/images/arrow-green-small.svg";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 
 const Section = ({ title, header }) => {
