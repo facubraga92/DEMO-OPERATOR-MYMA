307757e773fb869ad4d529f797b4b81f9d32c50f
FacundoBraga <facundobraga@hotmail.com>
2024-01-17 21:10:33 -0300

    Finished footer upgrades.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { styled } from "@mui/material";
+import { Grid, Typography, styled } from "@mui/material";
 
 // import { ReactComponent as LogoutIcon } from "../../assets/images/logout-icon.svg";
 import { ReactComponent as AlertsIcon } from "../../assets/images/notifications-icon.svg";
@@ -21,7 +21,7 @@ const FooterContainer = styled("div")((props) => {
     borderRadius: "20px 20px 0 0",
     border: "solid 2px white",
     borderBottom: "none",
-    padding: "15px 10px",
+    padding: "10px 10px",
     display: "flex",
     justifyContent: "space-between",
   };
@@ -33,13 +33,13 @@ const FooterButtons = styled("div")((props) => {
     height: 46,
     border: props.main ? "1px solid #FFF" : "none",
     borderRadius: "50%",
-    backgroundColor: props.active ? "#FFF" : "",
+    backgroundColor: props.active ? "#fff" : "",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     padding: "10px",
     "& svg": {
-      fill: props.active ? props.theme.palette.purple.main : "black",
+      fill: props.active ? props.theme.palette.purple.main : "#8C8C8C",
     },
   };
 });
@@ -70,42 +70,152 @@ const Footer = ({ activeIconID }) => {
 
   return (
     <FooterContainer>
-      <FooterButtons
-        className="joyride-step1"
-        active={0 === activeIconID}
-        onClick={() => handleButtonClick("home")}
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
       >
-        <HomeIcon />
-      </FooterButtons>
-      <FooterButtons
-        className="joyride-step2"
-        active={1 === activeIconID}
-        onClick={() => handleButtonClick("machines")}
+        <Grid item sx={{ display: "flex" }}>
+          <FooterButtons
+            className="joyride-step1"
+            active={0 === activeIconID}
+            onClick={() => handleButtonClick("home")}
+          >
+            <HomeIcon />
+          </FooterButtons>
+        </Grid>
+        <Grid item sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              color: activeIconID === 0 ? "#663B73" : "#8C8C8C",
+              fontWeight: "bold",
+            }}
+            variant="caption"
+          >
+            Portfolio
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
       >
-        <VendingMachineIcon />
-      </FooterButtons>
-      <FooterButtons
-        className="joyride-step3"
-        // main
-        active={2 === activeIconID}
-        onClick={() => handleButtonClick("experiments")}
+        <Grid item sx={{ display: "flex" }}>
+          <FooterButtons
+            className="joyride-step2"
+            active={1 === activeIconID}
+            onClick={() => handleButtonClick("machines")}
+          >
+            <VendingMachineIcon />
+          </FooterButtons>
+        </Grid>
+        <Grid item sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              color: activeIconID === 1 ? "#663B73" : "#8C8C8C",
+              fontWeight: "bold",
+            }}
+            variant="caption"
+          >
+            Máquinas
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
       >
-        <ExperimentsIcon fontSize="large" />
-      </FooterButtons>
-      <FooterButtons
-        className="joyride-step4"
-        active={3 === activeIconID}
-        onClick={() => handleButtonClick("cards")}
+        <Grid item sx={{ display: "flex", justifyContent: "center" }}>
+          <FooterButtons
+            className="joyride-step3"
+            // main
+            active={2 === activeIconID}
+            onClick={() => handleButtonClick("experiments")}
+          >
+            <ExperimentsIcon fontSize="large" />
+          </FooterButtons>
+        </Grid>
+        <Grid item sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              color: activeIconID === 2 ? "#663B73" : "#8C8C8C",
+              fontWeight: "bold",
+            }}
+            variant="caption"
+          >
+            Experimentos
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
       >
-        <AlertsIcon />
-      </FooterButtons>
-      <FooterButtons
-        className="joyride-step5"
-        active={4 === activeIconID}
-        onClick={() => handleButtonClick("insights")}
+        <Grid item sx={{ display: "flex" }}>
+          <FooterButtons
+            className="joyride-step4"
+            active={3 === activeIconID}
+            onClick={() => handleButtonClick("cards")}
+          >
+            <AlertsIcon />
+          </FooterButtons>
+        </Grid>
+        <Grid item sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography
+            sx={{
+              color: activeIconID === 3 ? "#663B73" : "#8C8C8C",
+              fontWeight: "bold",
+            }}
+            variant="caption"
+          >
+            Alertas
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
       >
-        <InsightsOutlinedIcon fontSize="large" />
-      </FooterButtons>
+        <Grid item sx={{ display: "flex" }}>
+          <FooterButtons
+            className="joyride-step5"
+            active={4 === activeIconID}
+            onClick={() => handleButtonClick("insights")}
+          >
+            <InsightsOutlinedIcon fontSize="large" />
+          </FooterButtons>
+        </Grid>
+        <Grid item sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              color: activeIconID === 4 ? "#663B73" : "#8C8C8C",
+              fontWeight: "bold",
+            }}
+            variant="caption"
+          >
+            Insights
+          </Typography>
+        </Grid>
+      </Grid>
     </FooterContainer>
   );
 };
